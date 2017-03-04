68cbce2dada3556b8e05275577a6f0875e5963d2
rachmadaniHaryono <foreturiga@gmail.com>
2017-03-05 06:50:59 +0800

    new: test: testing.
    - change setup for testing and to pass flake8
    - test some function on main program.
    - setting for travis-ci

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup
+
 setup(
-  name = 'playlistfromsong',
-  packages = ['playlistfromsong'], 
-  version = '0.9',
-  description = 'An offline music station generator',
-  author = 'schollz',
-  author_email = 'hypercube.platforms@gmail.com',
-  url = 'https://github.com/schollz/playlistfromsong',
-  download_url = 'https://github.com/schollz/playlistfromsong/archive/v0.9.tar.gz', 
-  keywords = ['music', 'youtube', 'playlist'], 
-  classifiers = [],
-  install_requires=[
-        "requests",
-        "youtube_dl",
-  ],
-  entry_points={'console_scripts': 
-        [
-        'playlistfromsong = playlistfromsong.__main__:main', 
-        ], 
-  },
+    name='playlistfromsong',
+    packages=['playlistfromsong'],
+    version='0.9',
+    description='An offline music station generator',
+    author='Zack Scholl',
+    url='https://github.com/schollz/playlistfromsong',
+    author_email='hypercube.platforms@gmail.com',
+    download_url='https://github.com/schollz/playlistfromsong/archive/v0.9.tar.gz',
+    keywords=['music', 'youtube', 'playlist'],
+    classifiers=[],
+    install_requires=[
+          "requests",
+          "youtube_dl",
+    ],
+    setup_requires=['pytest-runner==2.11.1'],
+    tests_require=[
+        'pytest-flake8==0.8.1',
+        'pytest==3.0.6',
+        'pytest-cov==2.4.0'
+
+    ],
+    entry_points={'console_scripts': [
+          'playlistfromsong = playlistfromsong.__main__:main',
+    ], },
 )
