0e40f20a91e3e63964a787a5500861d6b9d775e1
Zack Scholl <zack.scholl@gmail.com>
2017-03-06 07:43:31 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='playlistfromsong',
     packages=['playlistfromsong'],
-    version='0.14',
+    version='0.15',
     description='An offline music station generator',
     author='schollz',
     url='https://github.com/schollz/playlistfromsong',
     author_email='hypercube.platforms@gmail.com',
-    download_url='https://github.com/schollz/playlistfromsong/archive/v0.14.tar.gz',
+    download_url='https://github.com/schollz/playlistfromsong/archive/v0.15.tar.gz',
     keywords=['music', 'youtube', 'playlist'],
     classifiers=[],
     install_requires=[
