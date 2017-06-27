303b3e534cb73ea82152e1e567536d959aff1c9f
Zack Scholl <zack.scholl@gmail.com>
2017-06-27 14:50:37 -0600

    Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
+html_theme_options = {
+    'logo': 'logo.png',
+    'github_user': 'schollz',
+    'github_repo': 'playlistfromsong',
+}
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
