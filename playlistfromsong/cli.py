c295ac2749a222cca79ff29b9fa366e2af9fd98f
Zack Scholl <zack.scholl@gmail.com>
2017-10-10 07:31:26 -0600

    Fix errors with Python2 (stop using python2!)

--- playlistfromsong/cli.py
+++ playlistfromsong/cli.py
@@ -5,23 +5,23 @@
 import click
 from sys import exit
 from os import getcwd
-# import package future
-from builtins import input
 
 # For running as package
-from playlistfromsong import run, getTopFromLastFM
-from server import run_server
+from .playlistfromsong import run, getTopFromLastFM
+from .server import run_server
 
+from sys import version_info
 
 # # For running as script
 # from playlistfromsong import run
 # from server import run_server
 
+
 @click.command()
-@click.option('--num','-n', default=3, help='Number of songs.')
-@click.option('--song','-s', default=None, help='Artist + Song to seed.')
-@click.option('--bearer','-b', default=None, help='Bearer token for Spotify.')
-@click.option('--folder','-f', default=None, help='Folder to save files.')
+@click.option('--num', '-n', default=3, help='Number of songs.')
+@click.option('--song', '-s', default=None, help='Artist + Song to seed.')
+@click.option('--bearer', '-b', default=None, help='Bearer token for Spotify.')
+@click.option('--folder', '-f', default=None, help='Folder to save files.')
 @click.option('--serve', is_flag=True, help='Start personal web server.')
 @click.option('--port', default="5000", help='Internal port to run server (e.g. 5000).')
 def main(num, song, bearer, folder, serve, port):
@@ -32,17 +32,23 @@ def main(num, song, bearer, folder, serve, port):
         run_server(folder, port)
     elif song != None:
         while True:
-            recommendation_artist_song = getTopFromLastFM(song)
-            c = input("Did you mean \"%s\"? (y/n)\n" % (recommendation_artist_song))
+            song = getTopFromLastFM(song)
+            if (version_info > (3, 0)):
+                getinput = input
+            else:
+                getinput = raw_input
+            c = getinput('Did you mean "' +
+                         song + '"? (y/n) ')
             if c is "y":
                 num = 1
-                song = recommendation_artist_song
-                click.echo("Generating playlist for %d songs from '%s'" % (num, song))
+                click.echo(
+                    "Generating playlist for %d songs from '%s' \n(for generating more songs, use -n NUMBER)" % (num, song))
                 run(song.replace(" - ", " "), num, bearer=bearer, folder=folder)
                 break
-            song = input("Please enter the artist and song (e.g. The Beatles Let It Be): ")
+            song = getinput(
+                "Please enter the artist and song (e.g. The Beatles Let It Be): ")
     else:
         click.echo("Specify a song with --song 'The Beatles Let It Be'")
-        
+
 if __name__ == "__main__":
     main()
